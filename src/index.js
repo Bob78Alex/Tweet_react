5211e59d02df80067f4114940c1f1980994d88de
Alex Babenko <bob78alex@gmail.com>
2019-07-10 09:05:57 -0400

    Basic structure

--- src/index.js
+++ src/index.js
@@ -1,12 +1,112 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import moment from 'moment';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+function Tweet({ tweet }) {
+
+	return (
+		<div className = "tweet">
+		<Avatar hash={tweet.gravatar}/>
+		<div className = "content">
+		<NameWithHandle author={tweet.author}/><Time time={tweet.tweetstamp}/><br/>
+		<Message text={tweet.message}/>
+		<div className="buttons">
+			<ReplyButton/>
+			<RetweetButton count={tweet.retweets}/>
+			<LikeButton count={tweet.likes}/>
+			<MoreOptionsButton/>
+		</div>
+		</div>
+		</div>);
+}
+function Avatar({ hash }) {
+	var url = "https://www.gravatar.com/avatar/" + hash
+	return (
+		<img src={url}
+		className="avatar"
+		alt="avatar"/>
+	);
+}
+function Message({ text }) {
+	return (
+		<div className="message">
+		{ text }
+		</div>);
+					}
+function NameWithHandle({ author }) {
+	const { name, handle } = author;
+	return (
+		<span className = "name-with-handle">
+		<span className = "name">{name}</span>
+		<span className = "handle">@{handle}</span>
+		</span>);
+	}
+
+const Time = ({ time }) => {
+	const timeString = moment(time).fromNow();
+	return (
+		<span className="time">
+		{timeString}
+		</span>
+		);
+};
+
+const ReplyButton = () => (
+	<i className="fa fa-reply reply-button"/>
+	);
+
+function getRetweetCount(count) {
+	if (count >0) {
+		return (
+			<span className = "retweet-count">
+			{count}
+			</span>
+			);
+	} else {
+		return null;
+		}
+	}
+
+	
+const RetweetButton = ({ count }) => (
+	<span className="retweet-button">
+	<i className="fa fa-retweet"/>
+	{getRetweetCount(count)}
+	</span>
+	);
+
+
+
+const LikeButton = ({ count }) => (
+	<span className="like-button">
+	<i className="fa fa-heart"/>
+	{count > 0 && <span className="like-count">
+	{count}
+	</span>}
+	</span>
+	);
+
+
+
+
+const MoreOptionsButton = () => (
+	<i className="fa fa-ellipsis-h more-options-button"/>);
+
+var testTweet = {
+	message: "Something about cats.",
+	gravatar: "xyz",
+	author: {
+		handle: "catperson",
+		name: "Cat Person"
+	},
+	likes: 10,
+	retweets: 5,
+	tiestamp: "2019-07-30 21:24:37"
+};
+
+
+
+ReactDOM.render(<Tweet tweet={testTweet}/>, document.querySelector('#root'));
